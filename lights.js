568164b82c7b1de94403864d1a19475de9ea1446
Johnathan Miles <johnathanmiles@Johnathans-MacBook-Pro.local>
2015-07-17 21:29:19 +0100

    change for the message returned

--- lights.js
+++ lights.js
@@ -3,11 +3,11 @@ var lightcontrol = require('./lightcontrol.js');
 exports.turnOn = function(req, res) {
   console.log('Turning On');
   lightcontrol.switchSocket1(1);
-  res.send("OK");
+  res.send("On");
 };
 
 exports.turnOff = function(req, res) {
   console.log('Turning Off');
   lightcontrol.switchSocket1(0);
-  res.send("");
+  res.send("Off");
 };
